42d001d385ce85b5d6eac7508d50e4128bfe9434
nehahg3 <nehahg3@gmail.com>
2024-10-19 08:05:49 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
+import os
+from datetime import datetime
+import pytz
+import subprocess
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+@app.route('/htop')
+def htop():
+    full_name="NEHA HG"
+    username=os.getlogin()
+    ist=pytz.timezone('Asia/Bangalore')
+    server_time=datetime.now(ist).strftime('%Y-%m-%d %H:%M:%S')
+    try:
+        top_output=subprocess.check_output(['top','-b','-n','1']).decode('utf-8')
+    except Exception as e:
+        top_output=f"Error running 'top':{e}"
+    return render_template('index.html',full_name=full_name,username=username,server_time=server_time,top_output=top_output)
+    if __name__=='__main__':
+        app.run(host='0.0.0.0',port=5000)
+
+
